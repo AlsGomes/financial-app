59ec106eb0996c117808834e0bca85fd389e838e
AlsGomes <als_08.net@hotmail.com>
2021-08-02 18:01:00 -0300

    First Request Done

--- frontend/src/components/DataTable/index.jsx
+++ frontend/src/components/DataTable/index.jsx
@@ -1,6 +1,27 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import { BASE_URL } from "../../utils/requests";
+import { formatLocalDate } from "../../utils/format"
+
 const DataTable = () => {
 
-    
+    const [page, setPage] = useState({
+        first: true,
+        last: false,
+        number: 0,
+        totalElements: 0,
+        totalPages: 0
+    });
+
+    useEffect(() => {
+        async function fetchData() {
+            const res = await axios.get(`${BASE_URL}/records/page?id=2&page=0&linesPerPage=24&orderBy=date&direction=DESC`)
+            setPage(res.data)
+        }
+
+        fetchData()
+    }, [])
 
     return (
         <div className="table-responsive">
@@ -16,38 +37,16 @@ const DataTable = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>29/07/2021</td>
-                        <td>Compra de produtos de higiene e limpeza</td>
-                        <td>150.50</td>
-                        <td>DESPESA</td>
-                        <td>EFETIVADO</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>29/07/2021</td>
-                        <td>Compra de produtos de higiene e limpeza</td>
-                        <td>150.50</td>
-                        <td>DESPESA</td>
-                        <td>EFETIVADO</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>29/07/2021</td>
-                        <td>Compra de produtos de higiene e limpeza</td>
-                        <td>150.50</td>
-                        <td>DESPESA</td>
-                        <td>EFETIVADO</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>29/07/2021</td>
-                        <td>Compra de produtos de higiene e limpeza</td>
-                        <td>150.50</td>
-                        <td>DESPESA</td>
-                        <td>EFETIVADO</td>
-                    </tr>
+                    {page.content?.map(item => (
+                        <tr key={item.id}>
+                            <td>{item.id}</td>
+                            <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
+                            <td>{item.description}</td>
+                            <td>{item.value.toFixed(2)}</td>
+                            <td>{item.recordType}</td>
+                            <td>{item.recordStatus}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
